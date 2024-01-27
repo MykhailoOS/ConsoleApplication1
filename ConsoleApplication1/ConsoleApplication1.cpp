a660c7a3242db7b025cb9b4b60debda1fe15e2e9
Admin <Admin@DESKTOP-9GP61VV>
2024-01-27 11:09:17 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+// Функция с игровым циклом
+void game_state() {
+	const int NUMBER = rand() % 10 + 1;
+	int tries = 0;
+	int answer;
+	do
+	{
+		cout << "Ведите ваше число: \n";
+		cin >> answer;
+		tries += 1;
+		if (answer > NUMBER) {
+			cout << "Слишком далеко <<< /?\n\n";
+		}
+		else if (answer < NUMBER) {
+			cout << "Слишком далеко <<< /?\n\n";
+		}
+		else
+		{
+			cout << "Точно в цель!!!\nСделаные попытки: " << tries << "ответов!\n";
+		}
+	} while (tries!=answer);
+}
+
+
+
+
+// Главное меню...
+int main() {
+	setlocale(LC_ALL, "RU");
+	srand(static_cast<unsigned int>(time(0)));
+	cout << "\tFind number\n\n" << endl;
+	game_state();
+	return 0;
+}
